ad6a9aa39a3e167060b6a498fad0c2c82ae88506
203720 <203720@ids.upchiapas.edu.mx>
2021-11-27 01:19:39 -0600

    Create CrudFormOpinions.js
    Co-Authored-By: Emilio Torres <77992695+203453@users.noreply.github.com>

---
+++ proyecto-api/src/componentes/CrudFormOpinions.js
@@ -0,0 +1,62 @@
+import React, {useState, useEffect} from 'react';
+
+const initialForm={
+     usuario:"",
+     calificacion:null,
+     comentario:"",
+     id:null,
+};
+
+const CrudFormOpinions =({agregarRegistro, actualizarRegistro, datoEditar, setDatoEditar})=>{
+     const [form, setForm]=useState(initialForm);//state
+
+     useEffect(()=>{
+          if (datoEditar) {
+               setForm(datoEditar);//state
+          }else {
+               setForm(initialForm);//state
+          }
+     }, [datoEditar]);
+
+     const handleChange = (e)=>{//props
+          setForm({... form,[e.target.name]:e.target.value,});
+     };
+     
+     const handleSubmit = (e)=>{//props
+          e.preventDefault();
+          if(!form.usuario || !form.calificacion || !form.comentario){
+               alert("Datos incompletos");
+               return;
+          }
+
+          if (form.id===null) {
+               agregarRegistro(form);
+          }else{
+               actualizarRegistro(form);
+          }
+
+          handleReset();
+     };
+
+     const handleReset = (e)=>{//props
+          setForm(initialForm);
+          setDatoEditar(null);
+     };
+
+     return(
+                         <div class = "reseña">
+                              <h1 class="h1">Cuéntanos tú experiencia</h1>
+                              <form onSubmit={handleSubmit}>
+                                   <input type="text" name = "usuario" placeholder="usuario" onChange={handleChange} value={form.usuario}/>
+                                   <input type="number" name = "calificacion" min="0" max="10" placeholder="calificacion" onChange={handleChange} value={form.calificacion}/>
+                                   <input type="text" name = "comentario" placeholder="comentario" onChange={handleChange} value={form.comentario}/>
+                                   <div class="d-grid gap-2 d-md-block">
+                                        <button class= "btn btn-success" type="submit">Enviar</button>
+                                        <button class="btn btn-info" type="reset" onClick={handleReset}>Limpiar</button>
+                                   </div>
+                              </form>
+                         </div>
+     );
+};
+
+export default CrudFormOpinions
