1810deee61f62b9264d15daf8bd78b8d9111e2b5
203720 <203720@ids.upchiapas.edu.mx>
2021-11-27 01:19:35 -0600

    Create CrudApiOpinions.js
    Co-Authored-By: Emilio Torres <77992695+203453@users.noreply.github.com>

---
+++ proyecto-api/src/componentes/CrudApiOpinions.js
@@ -0,0 +1,118 @@
+import React from 'react'
+import {helpHttp} from "../helpers/helpHTTP";
+import  CrudFormOpinions  from './CrudFormOpinions';
+import CrudTableOpinions from "./CrudTableOpinions";
+
+
+const CrudApiOpinions = () => {
+     const [db, setDb]=useState(null);
+     const [datoEditar, setDatoEditar]=useState(null);
+     const [error, setError]=useState(null);
+     //const [loading, setLoading]= useState(false);
+
+
+     let api=helpHttp();
+
+     let url= "http://localhost:5000/opinions";
+
+    useEffect(()=> {
+        helpHttp().get(url).then((res)=>{
+            //setLoading(true);
+            console.log(res);
+
+            if(!res.err){
+                setDb(res);
+                setError(null);
+            }           
+            else{setDb(null);
+                setError(res);
+            }
+
+            //setLoading(false);
+
+        });
+    },[url]);
+
+    const agregarRegistro=(data)=>{
+       
+        data.id=Date.now();
+        let options={
+            body:data,
+            headers:{"content-type":"application/json"}
+        };
+
+        api.post(url,options).then((res)=>{
+            console.log(res);
+            if(!res.err){
+                setDb([...db,res]);
+            }else{
+                setDb(res);
+            }
+
+        });
+        setDb([...db,data]);
+    };
+
+    const actualizarRegistro=(data)=>{
+        let endpoint=`${url}/${data.id}`;
+        console.log(endpoint);
+    
+        let options={
+            body:data,
+            headers:{"content-type":"application/json"}
+        };
+
+        api.put(endpoint,options).then((res)=>{
+
+            if(!res.err){
+                let nuevoDato=db.map((el)=> (el.id=== data.id? data : el));
+                setDb(nuevoDato);
+            }else{
+                setDb(res);
+            }
+        });
+      
+    };
+
+    const borrarRegistro=(id)=>{
+        let borrar =window.confirm(
+            `¿Estas seguro de eliminar el registro '$(id)'?`
+        );
+
+        if(borrar){
+            let endpoint = `${url}/${id}`;
+            let options = {
+                headers:{"content-type":"application/json"}
+            };
+
+            api.del(endpoint,options).then((res)=>{
+                if(!res.err){
+                    let nuevoDato=db.filter((el)=>el.id !== id);
+                    setDb(nuevoDato);
+                }else{
+                    setDb(res);
+                }
+            });
+            
+        }else{
+            return;
+        }
+    };
+
+    return(
+        <div>
+            <CrudFormOpinions 
+                agregarRegistro={agregarRegistro}
+                actualizarRegistro={actualizarRegistro}
+                datoEditar={datoEditar}
+                setDatoEditar={setDatoEditar}
+            />
+
+            {db&& <CrudTableOpinions 
+            data={db}
+            setDatoEditar={setDatoEditar}
+            borrarRegistro={borrarRegistro}    
+          />}
+        </div>
+    )
+}
