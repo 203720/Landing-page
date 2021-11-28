e76f2761acee7ddc8f28a85e5bd5c61884591fd4
203720 <203720@ids.upchiapas.edu.mx>
2021-11-27 23:58:48 -0600

    Create Hombres.jsx
    Co-Authored-By: Emilio Torres <77992695+203453@users.noreply.github.com>

---
+++ proyecto-api/src/pages/Hombres.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import '../css/categorias.css'
+
+export const Hombres = () => {
+     return (
+          <div>
+  <h1>CABALLEROS</h1>
+  <div className="card-colums">
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/boina_h.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Boina - $189</h5>
+        <p className="card-text">Boina azul suiza<br />Colores: Azul / Negra<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen1.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Camisa colores - $199</h5>
+        <p className="card-text">Camisa patrón colores caballero<br />Tallas: XL - L - M - S<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen3.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Sueter Chaleco - $299</h5>
+        <p className="card-text">Sueter tipo chaleco caballero<br />Tallas: XL - L - M - S<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/boina_n2.jpeg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Boina - $199</h5>
+        <p className="card-text">Boina rosa palo unisex<br />Colores: Rosa palo<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+  </div>
+  <div className="card-colums">
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen9.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Camisa Algodón - $349</h5>
+        <p className="card-text">Camisa tela suave caballero<br />Tallas: XL - L - M - S<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen6.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Camisa Tirantes - $599</h5>
+        <p className="card-text">Camisa con tirantes pajarita<br />Tallas: XL - L - M - S<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen11.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Camisa Stripes - $199</h5>
+        <p className="card-text">Camisa rayas negras caballero<br />Tallas: XL - L - M - S<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/imagen7.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Pantalones hombre - $299</h5>
+        <p className="card-text">Pantalones vintage slim<br />Tallas: XL - L - M - S<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+  </div>
+  <footer><div className="socialLogos"><a href="https://www.facebook.com/profile.php?id=100069569205743" target="_blank"><img className="social" src="recursos/fb.png" width="44px" /></a></div><div className="socialLogos2"><a href="https://www.instagram.com/azul_briones/?hl=es" target="_blank"><img className="social" src="recursos/ig.png" width="40px" /></a></div><br />Azul Gissele Briones Zavala<br />Emilio Jarey Méndez Torres</footer>
+</div>
+
+     )
+}
+
+export default Hombres
