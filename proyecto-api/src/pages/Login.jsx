159e10fc43ac7efd12309547513da192ff3cef9e
203720 <203720@ids.upchiapas.edu.mx>
2021-11-28 00:32:21 -0600

    Update Login.jsx
    Co-Authored-By: Emilio Torres <77992695+203453@users.noreply.github.com>

--- proyecto-api/src/pages/Login.jsx
+++ proyecto-api/src/pages/Login.jsx
@@ -4,22 +4,7 @@ import '../css/login.css'
 export const Login = () => {
   return(
     <div>
-  <nav>
-    <ul>
-      <li className="menu"><a className="hvr-underline-from-center" href="home.html">INICIO</a></li>
-      <li className="menu"><a className="hvr-underline-from-center" href="#">CATEGORIAS</a>
-        <ul className="ani-menu">
-          <li><a className="hvr-underline-from-center" href="hombres.html">Caballeros</a></li>
-          <li><a className="hvr-underline-from-center" href="mujeres.html">Damas</a></li>
-          <li><a className="hvr-underline-from-center" href="ninos.html">Niños</a></li>
-        </ul>
-      </li>
-      <li className="menu"><a className="hvr-underline-from-center" href="/public/index.html">OPINIONES</a></li>
-      <li className="menu"><a className="hvr-underline-from-center" href="nosotros.html">SOBRE NOSOTROS</a></li>
-      <li className="menu"><a className="hvr-underline-from-center" href="compra.html"><img className="icon" src="recursos/carrito.png" /></a></li>
-      <li className="menu"><a className="hvr-underline-from-center" href="login.html"><img className="icon" src="recursos/login.png" /></a></li>
-    </ul>
-  </nav>
+  
   <div className="container">
     <div className="wrapper">
       <div className="cuadro">    
@@ -31,6 +16,7 @@ export const Login = () => {
               <input id="user" type="text" name="u" placeholder="Usuario" required="required" />
               <input id="pass" type="password" name="p" placeholder="Contraseña" required="required" />
               <button id="login" className="btn btn-primary btn-block btn-large" onclick="info()">Acceder</button>
+              <a  href="http://localhost:3000/Signin">Registrarse</a>
             </form>
           </div>
         </div>
