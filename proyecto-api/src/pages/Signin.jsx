6916887427929660871d7b6008ebe8a1b8768d45
203720 <203720@ids.upchiapas.edu.mx>
2021-11-28 14:54:08 -0600

    Update Signin.jsx
    Co-Authored-By: Emilio Torres <77992695+203453@users.noreply.github.com>

--- proyecto-api/src/pages/Signin.jsx
+++ proyecto-api/src/pages/Signin.jsx
@@ -3,18 +3,9 @@ import CrudApiUsers from '../componentes/CrudApiUsers';
 
 export const Signin = () => {
      return (
-<div>
-     <div className="container">
-          <div className="wrapper">
-               <div className="contact-wrapper-login">
-
-               </div>
-          </div>
-     </div>
      <>
     <CrudApiUsers/>
     </>
-</div>
      )
 }
 
