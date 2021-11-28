17a72a3aedcf41e7c65c798ce8a2820ba0832b7e
203720 <203720@ids.upchiapas.edu.mx>
2021-11-27 23:58:57 -0600

    Create Menores.jsx
    Co-Authored-By: Emilio Torres <77992695+203453@users.noreply.github.com>

---
+++ proyecto-api/src/pages/Menores.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import '../css/categorias.css'
+
+
+export const Menores = () => {
+     return (
+          <div>
+  <h1>NIÑOS</h1>
+  <div className="card-colums">
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen18.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Vestido niña - $199</h5>
+        <p className="card-text">Vestido café niña<br />Tallas: L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen17.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Conjunto niño - $299</h5>
+        <p className="card-text">Conjunto vintage niño<br />Tallas: L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/imagen19.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Conjunto niña - $399</h5>
+        <p className="card-text">Conjunto vintage niña<br />Tallas: L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/imagen14.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Sudadera unisex - $299</h5>
+        <p className="card-text">Sudadera colores unisex<br />Tallas: L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+  </div>
+  <div className="card-colums">
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen21.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Pantalon niña - $249</h5>
+        <p className="card-text">Pantalon mezclilla niña<br />Tallas: L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/boina_n.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Boina niño - $179</h5>
+        <p className="card-text">Boina niño patron stripes<br />Colores: Negro<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/vestido_n.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Vestido elegante - $599</h5>
+        <p className="card-text">Vestido verde elegante niña<br />Tallas: L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/camisa_n.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Camisa niño - $199</h5>
+        <p className="card-text">Camisa flores niño<br />Tallas: L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+  </div>
+  <footer><div className="socialLogos"><a href="https://www.facebook.com/profile.php?id=100069569205743" target="_blank"><img className="social" src="recursos/fb.png" width="44px" /></a></div><div className="socialLogos2"><a href="https://www.instagram.com/azul_briones/?hl=es" target="_blank"><img className="social" src="recursos/ig.png" width="40px" /></a></div><br />Azul Gissele Briones Zavala<br />Emilio Jarey Méndez Torres</footer>
+</div>
+
+     )
+}
+
+export default Menores
