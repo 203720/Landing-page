00ba1657c9215293db467cf45a8c0df27578783b
203720 <203720@ids.upchiapas.edu.mx>
2021-11-27 23:58:52 -0600

    Create Mujeres.jsx
    Co-Authored-By: Emilio Torres <77992695+203453@users.noreply.github.com>

---
+++ proyecto-api/src/pages/Mujeres.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import '../css/categorias.css'
+
+export const Mujeres = () => {
+     return (
+          <div>
+  <h1>DAMAS</h1>
+  <div className="card-colums">
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen13.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Vestido Rosa</h5>
+        <p className="card-text">Vestido rosa pastel<br />Tallas: XL - L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen2.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Vestido - $319</h5>
+        <p className="card-text">Vestido rosa con botónes<br />Tallas: XL - L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/boina_m.jpeg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Boina Militar - $299</h5>
+        <p className="card-text">Boina militar dama<br />Colores: Negro<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen4.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Vestido Stripes - $329</h5>
+        <p className="card-text">Vestido rayas blancas azules<br />Tallas: XL - L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+  </div>
+  <div className="card-colums">
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen5.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Vestido Negro - $349</h5>
+        <p className="card-text">Vestido negro holgado corto<br />Tallas: XL - L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/boina2.jpeg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Boina - $199</h5>
+        <p className="card-text">Boina rosa palo unisex<br />Colores: Rosa palo<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/pantalones_m.jpeg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Pantalones - $399</h5>
+        <p className="card-text">Pantalón mezclilla dama<br />Tallas: XL - L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/bolsa.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Bolsa - $499</h5>
+        <p className="card-text">Bolso elegante dama<br />Colores: Blanco con negro<br />Envío grátis</p>
+        <a href="#" className="btn btn-primary">Comprar</a>
+      </div>
+    </div>
+  </div>
+  <footer><div className="socialLogos"><a href="https://www.facebook.com/profile.php?id=100069569205743" target="_blank"><img className="social" src="recursos/fb.png" width="44px" /></a></div><div className="socialLogos2"><a href="https://www.instagram.com/azul_briones/?hl=es" target="_blank"><img className="social" src="recursos/ig.png" width="40px" /></a></div><br />Azul Gissele Briones Zavala<br />Emilio Jarey Méndez Torres</footer>
+</div>
+
+     )
+}
+
+export default Mujeres
