cf6419de60312dc021ee4ef8e7f2287a459b61e3
203720 <203720@ids.upchiapas.edu.mx>
2021-11-28 00:00:13 -0600

    Create Admin.jsx
    Co-Authored-By: Emilio Torres <77992695+203453@users.noreply.github.com>

---
+++ proyecto-api/src/pages/Admin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CrudApiUsers from '../Componentes/CrudApiUsers';
+
+export const Admin = () => {
+     return (
+<div>
+     <>
+    <CrudApiUsers/>
+    </>
+</div>
+     )
+}
+
+export default Admin
