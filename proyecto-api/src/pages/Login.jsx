cd102cce59840961fbce23ab66eb09359e56494d
203720 <203720@ids.upchiapas.edu.mx>
2021-11-28 18:49:28 -0600

    Update Login.jsx
    Co-Authored-By: Emilio Torres <77992695+203453@users.noreply.github.com>

--- proyecto-api/src/pages/Login.jsx
+++ proyecto-api/src/pages/Login.jsx
@@ -3,18 +3,22 @@ import '../css/login.css'
 
 export const Login = () => {
   return(
-    
+       <div class="container">
+        <div class="wrapper">
         <div className="contact-wrapper-login">
           <div className="login">
             <h1>Iniciar Sesión</h1>
             <br />
             <form method="post">
-              <input id="user" type="text" name="u" placeholder="Usuario" required="required" />
-              <input id="pass" type="password" name="p" placeholder="Contraseña" required="required" />
-              <button id="login" className="btn btn-primary btn-block btn-large" onclick="info()">Acceder</button>
+              <input className="inputLogin" id="user" type="text" name="u" placeholder="Usuario" required="required" />
+              <input className="inputLogin" id="pass" type="password" name="p" placeholder="Contraseña" required="required" />
+              <button id="login" class="btn btn-primary btn-block btn-large" onclick="info()">Acceder</button>
               <a  href="http://localhost:3000/Signin">Registrarse</a>
             </form>
           </div>
+        </div>
+         </div>
+                
         </div>
   )
 }
