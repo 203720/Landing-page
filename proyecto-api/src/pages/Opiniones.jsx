ca21ad0bc7af5e6036ae54f040237b78fbc0e06f
203720 <203720@ids.upchiapas.edu.mx>
2021-11-28 20:59:48 -0600

    Update Opiniones.jsx
    Co-Authored-By: Emilio Torres <77992695+203453@users.noreply.github.com>

--- proyecto-api/src/pages/Opiniones.jsx
+++ proyecto-api/src/pages/Opiniones.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import CrudApiOpinions from '../componentes/CrudApiOpinions';
+import '../css/opiniones.css'
 
 export const opiniones = () => {
      return (
 <div>
+     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"></link>
+     <link  rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></link>
      <>
     <CrudApiOpinions/>
     </>
