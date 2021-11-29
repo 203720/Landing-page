9047fd141c54eb93f3b14d7fdc2cbc31a03e5f51
203720 <203720@ids.upchiapas.edu.mx>
2021-11-29 00:45:15 -0600

    Update Mujeres.jsx

--- proyecto-api/src/pages/Mujeres.jsx
+++ proyecto-api/src/pages/Mujeres.jsx
@@ -5,73 +5,106 @@ export const Mujeres = () => {
      return (
        
           <div style={{backgroundImage:`url(recursos/mujeres.jpg)`}}>
-            <link  rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"></link>
   <h1>DAMAS</h1>
-  <div className="cardCategorias-colums">
-    <div className="cardCategorias" style={{width: '18rem'}}>
-      <img className="cardCategorias-img-top" src="recursos/Imagen13.jpg" alt="Card image cap" />
-      <div className="cardCategorias-body">
+  <div className="cardCate-colums">
+    <div className="cardCate" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen13.jpg" alt="Card image cap" />
+      <div className="card-body">
         <h5 className="card-title">Vestido Rosa</h5>
         <p className="card-text">Vestido rosa pastel<br />Tallas: XL - L - M - S - XS<br />Envío grátis</p>
-        <a href="#" className="btn btnCategorias-primary">Comprar</a>
+        <a href="#" className="btn btnCard-primary">Comprar</a>
       </div>
     </div>
-    <div className="cardCategorias" style={{width: '18rem'}}>
-      <img className="cardCategorias-img-top" src="recursos/Imagen2.jpg" alt="Card image cap" />
-      <div className="cardCategorias-body">
+    <div className="cardCate" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen2.jpg" alt="Card image cap" />
+      <div className="card-body">
         <h5 className="card-title">Vestido - $319</h5>
         <p className="card-text">Vestido rosa con botónes<br />Tallas: XL - L - M - S - XS<br />Envío grátis</p>
-        <a href="#" className="btn btnCategorias-primary">Comprar</a>
+        <a href="#" className="btn btnCard-primary">Comprar</a>
       </div>
     </div>
-    <div className="cardCategorias" style={{width: '18rem'}}>
-      <img className="cardCategorias-img-top" src="recursos/boina_m.jpeg" alt="Card image cap" />
-      <div className="cardCategorias-body">
-        <h5 className="cardCategorias-title">Boina Militar - $299</h5>
-        <p className="cardCategorias-text">Boina militar dama<br />Colores: Negro<br />Envío grátis</p>
-        <a href="#" className="btn btnCategorias-primary">Comprar</a>
+    <div className="cardCate" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/boina_m.jpeg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Boina Militar - $299</h5>
+        <p className="card-text">Boina militar dama<br />Colores: Negro<br />Envío grátis</p>
+        <a href="#" className="btn btnCard-primary">Comprar</a>
       </div>
     </div>
-    <div className="cardCategorias" style={{width: '18rem'}}>
-      <img className="cardCategorias-img-top" src="recursos/Imagen4.jpg" alt="Card image cap" />
-      <div className="cardCategorias-body">
-        <h5 className="cardCategorias-title">Vestido Stripes - $329</h5>
-        <p className="cardCategorias-text">Vestido rayas blancas azules<br />Tallas: XL - L - M - S - XS<br />Envío grátis</p>
-        <a href="#" className="btn btnCategorias-primary">Comprar</a>
+    <div className="cardCate" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen4.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Vestido Stripes - $329</h5>
+        <p className="card-text">Vestido rayas blancas azules<br />Tallas: XL - L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btnCard-primary">Comprar</a>
       </div>
     </div>
   </div>
-  <div className="cardCategorias-colums">
-    <div className="cardCategorias" style={{width: '18rem'}}>
-      <img className="cardCategorias-img-top" src="recursos/Imagen5.jpg" alt="Card image cap" />
-      <div className="cardCategorias-body">
-        <h5 className="cardCategorias-title">Vestido Negro - $349</h5>
-        <p className="cardCategorias-text">Vestido negro holgado corto<br />Tallas: XL - L - M - S - XS<br />Envío grátis</p>
-        <a href="#" className="btn btnCategorias-primary">Comprar</a>
+  <div className="cardCate-colums">
+    <div className="cardCate" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen5.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Vestido Negro - $349</h5>
+        <p className="card-text">Vestido negro holgado corto<br />Tallas: XL - L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btnCard-primary">Comprar</a>
       </div>
     </div>
-    <div className="cardCategorias" style={{width: '18rem'}}>
-      <img className="cardCategorias-img-top" src="recursos/boina2.jpeg" alt="Card image cap" />
-      <div className="cardCategorias-body">
-        <h5 className="cardCategorias-title">Boina - $199</h5>
-        <p className="cardCategorias-text">Boina rosa palo unisex<br />Colores: Rosa palo<br />Envío grátis</p>
-        <a href="#" className="btn btnCategorias-primary">Comprar</a>
+    <div className="cardCate" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/boina2.jpeg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Boina - $199</h5>
+        <p className="card-text">Boina rosa palo unisex<br />Colores: Rosa palo<br />Envío grátis</p>
+        <a href="#" className="btn btnCard-primary">Comprar</a>
       </div>
     </div>
-    <div className="cardCategorias" style={{width: '18rem'}}>
-      <img className="cardCategorias-img-top" src="recursos/pantalones_m.jpeg" alt="Card image cap" />
-      <div className="cardCategorias-body">
-        <h5 className="cardCategorias-title">Pantalones - $399</h5>
+    <div className="cardCate" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/pantalones_m.jpeg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Pantalones - $399</h5>
         <p className="card-text">Pantalón mezclilla dama<br />Tallas: XL - L - M - S - XS<br />Envío grátis</p>
-        <a href="#" className="btn btnCategorias-primary">Comprar</a>
+        <a href="#" className="btn btnCard-primary">Comprar</a>
       </div>
     </div>
-    <div className="cardCategorias" style={{width: '18rem'}}>
-      <img className="cardCategorias-img-top" src="recursos/bolsa.jpg" alt="Card image cap" />
-      <div className="cardCategorias-body">
-        <h5 className="cardCategorias-title">Bolsa - $499</h5>
-        <p className="cardCategorias-text">Bolso elegante dama<br />Colores: Blanco con negro<br />Envío grátis</p>
-        <a href="#" className="btn btnCategorias-primary">Comprar</a>
+    <div className="cardCate" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/bolsa.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Bolsa - $499</h5>
+        <p className="card-text">Bolso elegante dama<br />Colores: Blanco con negro<br />Envío grátis</p>
+        <a href="#" className="btn btnCard-primary">Comprar</a>
+      </div>
+    </div>
+  </div>
+    <div className="cardCate-colums">
+    <div className="cardCate" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen13.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Vestido Rosa</h5>
+        <p className="card-text">Vestido rosa pastel<br />Tallas: XL - L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btnCard-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="cardCate" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen2.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Vestido - $319</h5>
+        <p className="card-text">Vestido rosa con botónes<br />Tallas: XL - L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btnCard-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="cardCate" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/boina_m.jpeg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Boina Militar - $299</h5>
+        <p className="card-text">Boina militar dama<br />Colores: Negro<br />Envío grátis</p>
+        <a href="#" className="btn btnCard-primary">Comprar</a>
+      </div>
+    </div>
+    <div className="cardCate" style={{width: '18rem'}}>
+      <img className="card-img-top" src="recursos/Imagen4.jpg" alt="Card image cap" />
+      <div className="card-body">
+        <h5 className="card-title">Vestido Stripes - $329</h5>
+        <p className="card-text">Vestido rayas blancas azules<br />Tallas: XL - L - M - S - XS<br />Envío grátis</p>
+        <a href="#" className="btn btnCard-primary">Comprar</a>
       </div>
     </div>
   </div>
